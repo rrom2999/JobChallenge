62d5f169ac1f4d2e8ef4fa91341a2e690a003440
Ronald <rovalle@bluedevs.com>
2022-01-17 16:34:59 -0600

    [Places]: getPlaces method done, local database

--- src/controllers/places.controller.js
+++ src/controllers/places.controller.js
@@ -1,10 +1,23 @@
+const {Pool} = require('pg');
+
+//Local access to Database
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'pg2999Dlanor',
+    database: 'inlandchallenge',
+    port:'5432'
+});
+
 //Methods
 const getPlaces = async (req, res) =>{
-    console.log('Get places...');
-    res.status(200).json({message: 'Get places'})
+    console.log('getPlaces ----------------');
+    const response = await pool.query('SELECT * FROM places');
+    console.log(response.rows);
+    res.status(200).json(response.rows);
 };
 
 
 module.exports = {
     getPlaces
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//For json and forms management
+app.use(express.json());
+app.use (express.urlencoded({extended: false}));
+
 //Routes
 app.use(require('./routes/router'));
 
